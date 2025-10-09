678cfb46fa29ea9b18ab8d6fd516737885fd874e
val-coding <valewanja06@gmail.com>
2025-10-09 14:11:24 +0300

    Add files via upload

---
+++ Descending_order.c
@@ -0,0 +1,18 @@
+/*
+Name: Valentine Wanja Patrick 
+Reg no: CT101/G/26498/25
+Description: Descending order
+*/
+
+//pre proceesor directive
+#include <stdio.h>
+int main(){
+
+// for loop
+for(int i =100;i>=50;i--)
+{
+printf("%d \n",i);
+}
+return 0;
+}	
+		
